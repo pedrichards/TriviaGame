6ebc84aa432c779b0bb7746f560a6c291e930d95
Schisandra <pedrichards@gmail.com>
2019-07-27 18:42:53 -0400

    Created html

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    $("#start").on("click", start);
+  };
+
+  function start() {
+
+    // DONE: Use setInterval to start the count here and set the clock to running.
+    // if (!clockRunning) {
+    //   intervalId = setInterval(count, 1000);
+    //   clockRunning = true;
+    // }
+  }
